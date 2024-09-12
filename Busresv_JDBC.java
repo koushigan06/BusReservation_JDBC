cc0b6225c5a8aa27ee2bef2fa6a25a41f6c06de3
koushigan06 <130557434+koushigan06@users.noreply.github.com>
2024-09-12 11:27:11 +0530

    Update Busresv_JDBC.java

--- Busresv_JDBC.java
+++ Busresv_JDBC.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Busresv_JDBC {
     private static final String url="jdbc:mysql://localhost:3306/busresv";
     private static final String userName="root";
-    private static final String password="Chinni@6+7";
+    private static final String password="**********";
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(url, userName, password);
     }
